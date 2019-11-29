d8c2248b4fd8dbb471c5008978f117ae4ff4c998
João Almeida <joaorsalmeida@gmail.com>
2019-11-29 14:09:19 -0300

    Solve manufacturers index

---
+++ app/controllers/manufacturers_controller.rb
@@ -0,0 +1,10 @@
+class ManufacturersController < ApplicationController
+  def index
+    @manufacturers = Manufacturer.all
+  end
+
+  def show
+    id = params[:id]
+    @manufacturer = Manufacturer.find(id)
+  end
+end
