3ef433391c53b6988f0ae884b569324c9aa7cb90
Henrique Morato <henrique.morato@campuscode.com.br>
2019-12-02 18:02:31 -0300

    Edit and validate manufacturer

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -11,11 +11,30 @@ class ManufacturersController < ApplicationController
   def new
     @manufacturer = Manufacturer.new
   end
-  
-  def create
-    @manufacturer = Manufacturer.create(manufacturer_params)
 
-    redirect_to @manufacturer
+  def edit
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+
+    if @manufacturer.save
+      redirect_to @manufacturer
+    else
+      flash.now[:alert] = 'Você deve preencher todos os campos'
+      render :new
+    end
+  end
+
+  def update
+    @manufacturer = Manufacturer.find(params[:id])
+    if @manufacturer.update(manufacturer_params)
+      flash[:notice] = 'Fabricante atualizada com sucesso'
+      redirect_to @manufacturer
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,2 +1,4 @@
 class Manufacturer < ApplicationRecord
+  validates :name, presence: { message: 'não pode ficar em branco' }
+  validates :name, uniqueness: { message: 'Nome já está em uso' }
 end
